fe3dd27ce8b3a76706fdf4605ced5e6a63cab299
Koen Vervoordeldonk <koenvervoordeldonk93@gmail.com>
2016-03-12 11:15:35 +0100

    Added small pokemon list

--- www/js/app.js
+++ www/js/app.js
@@ -5,20 +5,65 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('pokedex', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .controller('HomeCtrl', function($scope, $ionicModal) {
+    // Test data
+    $scope.pokemons = [
+      { name: 'Charizard' },
+      { name: 'Mewtwo' },
+      { name: 'Blastoise' },
+      { name: 'Mew' },
+      { name: 'Lugia' },
+      { name: 'Arcanine' },
+      { name: 'Dragonite' },
+      { name: 'Rayquaza' },
+      { name: 'Typhlosion' }
+    ];
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    //// No need for testing data anymore
+    //$scope.pokemons = [];
+
+    // Create and load the Modal
+    $ionicModal.fromTemplateUrl('new-pokemon.html', function(modal) {
+      $scope.pokemonModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    // Called when the form is submitted
+    $scope.createPokemon = function(pokemon) {
+      $scope.pokemons.push({
+        name: pokemon.name
+      });
+      $scope.pokemonModal.hide();
+      pokemon.name = "";
+    };
+
+    // Open our new pokemon modal
+    $scope.newPokemon = function() {
+      $scope.pokemonModal.show();
+    };
+
+    // Close the new pokemon modal
+    $scope.closeNewPokemon = function() {
+      $scope.pokemonModal.hide();
+    };
+  })
+
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+});
